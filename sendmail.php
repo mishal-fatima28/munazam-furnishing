0aec38e4e459ee4aae22ab5826488f5d45145d4b
mishal-fatima28 <mishalfatima28@gmail.com>
2022-08-19 17:05:43 +0500

    first commit

---
+++ sendmail.php
@@ -0,0 +1,48 @@
+<?php 
+
+header('Content-type: text/plain');
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['username'];
+	$useremail = $_POST['email'];
+	$message = $_POST['message'];
+
+	try{
+
+		$mail->isSMTP();
+		$mail->Host = 'smtp.gmail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'munazamfurnishing@gmail.com';
+		$mail->Password = 'yyxjpemduqwoxreb';
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->Port = 587;
+
+		$mail->setFrom('munazamfurnishing@gmail.com');
+		$mail->addAddress('mishalfatima28@gmail.com');
+
+		$mail->isHTML(true);
+		$mail->Subject = 'Munazam Furnishing Query';
+		$mail->Body = 'Name : ' .$name.'<br />'  . 'Email : ' .$useremail. '<br />' . "Message : " . $message;
+
+		$mail->send();
+		header("Location: contact-success.html");
+
+
+	} catch(Exception $e){
+
+		echo 'Email Not Sent';
+	}
+
+}
+
+
+ ?>
